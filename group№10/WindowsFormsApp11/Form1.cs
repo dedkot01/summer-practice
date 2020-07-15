ba1f386aa97128052264525b969502235e8ea6fc
“Alina <alinader02@mail.ru>
2020-07-15 20:58:00 +0300

    uploaded into repository

---
+++ group№10/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public void fout(string str, string text)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(@str);
+                sw.WriteLine(text);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public void generation()
+        {
+            listBox1.Items.Clear();
+            Random rand = new Random();
+            int c = 100, k = rand.Next() % c;
+            int[] arr = new int[c];
+            string text = " ", temp;
+            listBox1.Items.Add("№ значение массива");
+            for (int i = 0; i < c; i++)
+            {
+                arr[i] = k + (rand.Next() % c);// заполняем случайными числами ячейки
+                temp = Convert.ToString(i);
+                if (i < 10)
+                    temp += " ";
+                else
+                    temp += " ";
+                listBox1.Items.Add(temp + Convert.ToString(arr[i]));
+                text += Convert.ToString(arr[i]) + " ";
+                k = arr[i];
+            }
+            fout("Сортировка.txt", text);
+        }
+
+        public void sort1()
+        {
+            string text = "", num = "", result = "";
+            StreamReader f = new StreamReader("Сортировка.txt");
+            while (!f.EndOfStream)
+            {
+                text = f.ReadLine();
+            }
+            f.Close();
+            int j = 1, c = 100, flag = 0;
+            for (int i = 0; i < c; i++)
+            {
+                num = "";
+                while (text[j] != ' ')
+                {
+                    num += text[j];
+                    j++;
+                }
+                j++;
+                if (num == textBox1.Text)
+                {
+                    result += "Ключ " + textBox1.Text + " находится в ячейке " + i + "\n";
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+                result = "Мы не нашли ключ " + textBox1.Text + " в массиве";
+
+            label3.Text = result;
+            StreamWriter sw = new StreamWriter(@"Результат.txt");
+            sw.WriteLine(result);
+            sw.Close();
+        }
+
+        public void sort2()
+        {
+            string text = "", num = "", result = "";
+            StreamReader f = new StreamReader("Сортировка.txt");
+            while (!f.EndOfStream)
+            {
+                text = f.ReadLine();
+            }
+            f.Close();
+            int j = 1, c = 100;
+            int[] arr = new int[c];
+            for (int i = 0; i < c; i++)
+            {
+                num = "";
+                while (text[j] != ' ')
+                {
+                    num += text[j];
+                    j++;
+                }
+                j++;
+                arr[i] = Convert.ToInt32(num);
+
+            }
+            int l = 0, r = c, mid;
+            while (l < r)
+            {
+                mid = (l + r) / 2;
+                if (arr[mid] > Convert.ToInt32(textBox1.Text))
+                    r = mid;
+                else l = mid + 1;
+            }
+            r--;
+            if (arr[r] == Convert.ToInt32(textBox1.Text))
+            {
+                result += "Ключ " + textBox1.Text + " находится в ячейке " + r + "\n";
+            }
+            else
+            {
+                result = "Мы не нашли ключ " + textBox1.Text + " в массиве";
+            }
+            label3.Text = result;
+            StreamWriter sw = new StreamWriter(@"Результат.txt");
+            sw.WriteLine(result);
+            sw.Close();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            generation();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sort1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            sort2();
+        }
+    }
+}
